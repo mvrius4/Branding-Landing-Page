bad6c37ea41e4199c4a181ff7249f1c66659442c
mvrius69 <xmariusro@gmail.com>
2021-12-31 11:27:31 +0200

    added scroll reveal animations

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,17 @@ const nav = document.querySelector("nav");
 navToggle.addEventListener("click", () => {
     nav.classList.toggle("active");
 })
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '75px',
+    duration: 1250,
+    delay: 250
+});
+
+scrollReveal.reveal('main');
+scrollReveal.reveal('#info')
+scrollReveal.reveal('#services', { origin: 'right' });
+scrollReveal.reveal('#skills', { origin: 'left' });
+scrollReveal.reveal('#team', { origin: 'top' });
+scrollReveal.reveal('#contact', { origin: 'bottom' });
